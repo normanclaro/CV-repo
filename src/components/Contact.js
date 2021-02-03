7cf19291e9d46a10e01d38b16c0ae5c6eb9806f1
normanclaro <norman.claro85@gmail.com>
2021-02-02 22:56:51 -0300

    Add files via upload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,13 +6,14 @@ export default function Contact(){
         age: 35,
         mail: "norman.claro85@gmail.com",
         phone: "+5491151152663"
-    
+        
       };
     return(
         <div className="contact">
             <h2> {contact.name} </h2>
             <p> {contact.mail} </p>
-            <p> {contact.phone} </p>  
+            <p> {contact.phone} </p> 
+            <a href="https://github.com/normanclaro/CV-repo" target= "_blank" className="linkRep" >Repository</a> 
         </div>
     )
 };
